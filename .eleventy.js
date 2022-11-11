b07db4972b41bc73c1c91ac64e9832eb69f9f1ad
EH2O <edwin.holgerssonolofsson@elev.ga.ntig.se>
2022-11-11 10:18:53 +0100

    :)

--- .eleventy.js
+++ .eleventy.js
@@ -17,9 +17,30 @@ async function imageShortcode(src, alt, sizes) {
     // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
     return Image.generateHTML(metadata, imageAttributes);
 }
+function lazyImages (eleventyConfig, userOptions = {}) {
+    const {parse} = require('node-html-parser')
+  
+    const options = {
+      name: 'lazy-images',
+      ...userOptions
+    }
+  
+    eleventyConfig.addTransform(options.extensions, (content, outputPath) => {
+      if (outputPath.endsWith('.html')) {
+        const root = parse(content);
+        const images = root.querySelectorAll('img');
+        images.forEach((img) => {
+          img.setAttribute('loading', 'lazy')
+        })
+        return root.toString()
+      }
+      return content;
+    })
+  }
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+    eleventyConfig.addPlugin(lazyImages, {})
     return {
         dir: {
             input: 'src',
